d6158d2be43d12d6537dc45c20100228de02913f
isafck <insaneesz1e@gmail.com>
2024-07-08 16:22:42 +0530

    url routing

---
+++ .history/mysite/blog/views_20240708161953.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render 
+from .models import Post 
+def post(request):
+    post =Post.object.a()
---
+++ .history/mysite/blog/views_20240708162001.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render 
+from .models import Post 
+def post(request):
+    post =Post.objects.all();
+    
